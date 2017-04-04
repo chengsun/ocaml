bdba804f980cf2d36cbced40e87cfc0f5b24289a
Cheng Sun <chengsun9@gmail.com>
2017-04-04 16:26:25 +0100

    working typelibrary

--- bytecomp/liballocs.ml
+++ bytecomp/liballocs.ml
@@ -27,6 +27,7 @@ module C = struct
     | C_Boxed (* the union boxed_pointer_t *)
     | C_Void
     | C_Int
+    | C_Double
     | C_UInt
     | C_Bool
     | C_Char
@@ -49,7 +50,7 @@ module C = struct
     | C_Cast of ctype * expression
     | C_BinaryOp of string * expression * expression
     | C_FunCall of expression * expression list
-    | C_Allocate of int (* number of words *) * (Types.type_expr, string) result
+    | C_Allocate of ctype * int(*multiplier*) * (Types.type_expr, string) result (* for debugging only *)
 
   and statement =
     | C_Expression of expression
@@ -76,12 +77,16 @@ module C = struct
     | C_FunDefn _ -> t
 
 
+  (* the type library is responsible for translating type_exprs to C types,
+   * and keeping track of all newly created C types (structs), and the mapping
+   * from type_exprs to these types *)
   module TypeLibrary = struct
     (* the payload of data we want with each type_expr *)
     type t = ctype
 
     module TypeHash = Hashtbl.Make(Types.TypeOps)
-    let table: t TypeHash.t = TypeHash.create 16
+    (* TODO: store separate list in dependency order *)
+    let g_table: t TypeHash.t = TypeHash.create 16
 
     let rec tfield_to_list type_expr =
       match type_expr.desc with
@@ -95,7 +100,7 @@ module C = struct
         C_Struct (struct_name, (List.map (fun (fieldname, fieldtype) ->
           ocaml_to_c_type fieldtype, Ident.create fieldname) ts))
       in
-      TypeHash.add table type_expr ctype;
+      TypeHash.add g_table type_expr ctype;
       ctype
 
     and add_mapping type_expr =
@@ -110,10 +115,14 @@ module C = struct
       match type_expr.desc with
       | Tvar _ -> C_Boxed
       | Tarrow _ -> C_FunPointer (C_Void, []) (* TODO: could add more detail *)
+      | Tconstr (path, [], _) when path = Predef.path_int -> C_Int
+      | Tconstr (path, [], _) when path = Predef.path_string -> C_Pointer C_Char
+      | Tconstr (path, [], _) when path = Predef.path_float -> C_Double
+      (* TODO: add more *)
       | Tconstr _ -> C_Boxed (* TODO: who knows? *)
       | Ttuple _
       | Tobject _ ->
-          (try TypeHash.find table type_expr
+          (try TypeHash.find g_table type_expr
            with Not_found -> add_mapping type_expr)
       | Tfield _ | Tnil -> failwith "unexpected Tfield/Tnil at top level"
       | Tlink _e
@@ -123,8 +132,9 @@ module C = struct
 
 
     (* get all struct declarations so far *)
+    (* TODO: read from separate list in dependency order *)
     let dump_all_types_for_definition () =
-      TypeHash.fold (fun _k v acc -> v::acc) table []
+      TypeHash.fold (fun _k v acc -> v::acc) g_table []
   end
 
 
@@ -158,12 +168,19 @@ module C = struct
     | C_Boxed -> "boxed_pointer_t"
     | C_Void -> "void"
     | C_Int -> "intptr_t"
+    | C_Double -> "double"
     | C_UInt -> "unsigned"
     | C_Bool -> "bool"
     | C_Char -> "char"
     | C_Struct (id, _) -> "struct " ^ (Ident.unique_name id)
     | C_FunPointer (tyret, tyargs) -> Printf.sprintf "%s(*)(%s)" (ctype_to_string tyret) (map_intersperse_concat ctype_to_string "," tyargs)
     | C_VarArgs -> "..."
+
+  let rec ctype_size_nwords = function
+    | C_Pointer _ | C_FunPointer _ | C_Boxed | C_Int | C_Double | C_UInt -> 1
+    | C_Struct (_, tys) -> List.fold_left (fun acc (t,_) -> acc + (ctype_size_nwords t)) 0 tys
+    | C_Bool | C_Char -> failwith "bools/chars have sub-word size"
+    | C_VarArgs | C_Void -> failwith "invalid ctype to query size for"
 
   let cstruct_defn_string id fields =
     let fieldstrings =
@@ -208,11 +225,14 @@ module C = struct
     | C_BinaryOp (op,x,y) -> "(" ^ (expression_to_string x) ^ op ^ (expression_to_string y) ^ ")"
     | C_FunCall (e_id,es) ->
         (expression_to_string e_id) ^ "(" ^ (map_intersperse_concat expression_to_string ", " es) ^ ")"
-    | C_Allocate (n, tyinfo) -> Printf.sprintf "malloc(sizeof(%s)*%d /*%s*/)" (ctype_to_string C_Boxed) n
-            (match tyinfo with
-             | Ok expr -> formats Printtyp.type_expr expr
-             | Error s -> Printf.sprintf "NO TYPE INFO: %s" s
-            )
+    | C_Allocate (ctype, n, tyinfo) -> begin
+      let comment =
+        match tyinfo with
+        | Ok type_expr -> formats Printtyp.type_expr type_expr
+        | Error s -> Printf.sprintf "NO TYPE INFO: %s" s
+      in
+      Printf.sprintf "malloc(sizeof(%s)*%d /*%s*/)" (ctype_to_string ctype) n comment
+    end
 
   and statement_to_string = function
     | C_Expression e -> (expression_to_string e) ^ ";"
@@ -266,6 +286,19 @@ end
 open C
 
 let compile_implementation modulename lambda =
+  let do_allocation nwords (type_expr_result : _ result) =
+    let ctype, n =
+      match type_expr_result with
+      | Ok type_expr ->
+          let ctype = TypeLibrary.ocaml_to_c_type type_expr in
+          let ctype_size = ctype_size_nwords ctype in
+          assert (ctype_size = nwords);
+          ctype, 1
+      | Error _ -> C_Boxed, nwords
+    in
+    C_Allocate(ctype, n, type_expr_result)
+  in
+
   let make_boxed_d e =
       C_Cast (C_Boxed,
         (C_InitialiserList [ (* TODO: rewrite -- this is disgusting *)
@@ -350,7 +383,7 @@ let compile_implementation modulename lambda =
               in
               C_InlineRevStatements (construct 0 [
                   C_VarDeclare (C_Pointer C_Boxed, C_Variable id,
-                                Some (C_Allocate (List.length contents, tyinfo)))
+                                Some (do_allocation (List.length contents) tyinfo))
                 ] contents)
           | Psequand, [e1;e2] -> bop "&&" e1 e2
           | Psequor, [e1;e2] -> bop "||" e1 e2
@@ -559,6 +592,7 @@ let compile_implementation modulename lambda =
         ]
       ) !globals)
   in
+  (* NB: should be evaluated last when we know all types *)
   let global_typedecls =
     List.map (fun ty -> C_TypeDefn ty) (TypeLibrary.dump_all_types_for_definition ())
   in
