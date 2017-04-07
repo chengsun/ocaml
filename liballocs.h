6414fca1b4597539cd26614cbc800071c0b9e93f
Cheng Sun <chengsun9@gmail.com>
2017-04-07 18:14:58 +0100

    programs need string.h for strcmp

--- liballocs.h
+++ liballocs.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <setjmp.h>    // for setjmp
+#include <string.h>    // for strcmp
 
 #define STATIC_ASSERT(COND,MSG) typedef char static_assertion_##MSG[(COND)?1:-1]
 
