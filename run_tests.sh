f39565031c58098dde63de1fd77878f2a98b0bac
Cheng Sun <chengsun9@gmail.com>
2017-04-07 22:37:14 +0100

    run tests less verbose

--- run_tests.sh
+++ run_tests.sh
@@ -14,7 +14,7 @@ for ml in $TESTS; do
 
     echo ">>> Testing ${name}..."
     cp ./tests/${name}.ml /tmp/test.ml
-    ./ocamlc -dlambda -target-liballocs -o /dev/null -g /tmp/test.ml
+    ./ocamlc -target-liballocs -o /dev/null -g /tmp/test.ml
     gcc -ggdb -std=c99 -Wall -I. -o /tmp/test ./liballocs.c /tmp/test.c
     output=$(/tmp/test 2>&1) || true
     echo "$output" | diff -u /dev/stdin ./tests/${name}.expected || (echo ">>> Test failed!"; exit 1)
