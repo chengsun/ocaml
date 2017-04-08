45122b53feb52ac65e02ac65f2d3cfbf9409e985
Cheng Sun <chengsun9@gmail.com>
2017-04-08 17:17:08 +0100

    add bench

---
+++ bench/p153.ml
@@ -0,0 +1,47 @@
+let max = 1_000_000
+
+let list_fold_range max ~f =
+  let rec loop accum i =
+    if i > max
+    then accum
+    else loop (f accum i) (succ i)
+  in
+  loop 0 1
+;;
+
+let realdivs =
+    list_fold_range max ~f:(fun accum divisor -> accum + divisor * (max / divisor))
+;;
+
+let rec gcd a b =
+    match b with
+    | 0 -> a
+    | _ -> gcd b (a mod b)
+;;
+
+exception Done of int
+
+let gaussdivs =
+    list_fold_range max ~f:(fun accum a ->
+        accum + (
+          try
+            list_fold_range max ~f:(fun accum b ->
+                if gcd a b > 1 then accum
+                else (
+                  let divisor = a * a + b * b in
+                  if divisor > max then raise (Done accum)
+                  else
+                      let rec multipleloop accum k =
+                          if k * divisor > max then accum
+                          else multipleloop (accum + 2 * k * a * (max / (k * divisor))) (succ k)
+                      in
+                      multipleloop accum 1
+                )
+            );
+            raise Not_found
+          with Done x -> x
+        )
+    )
+;;
+
+let () = Printf.printf "%d + %d = %d\n" realdivs gaussdivs (realdivs + gaussdivs)
--- benchmark.sh
+++ benchmark.sh
@@ -6,7 +6,7 @@ name=$1
 
 echo ">>> Benchmarking ${name}..."
 cp ./bench/${name}.ml /tmp/test.ml
-./ocamlc -dlambda -target-liballocs -o /dev/null -g /tmp/test.ml
+./ocamlc -dlambda -target-liballocs -o /tmp/test.ocaml.exe -g /tmp/test.ml
 gcc -ggdb -std=c99 -Wall -I. -o /tmp/test ./liballocs.c /tmp/test.c
 tmp/test
 
