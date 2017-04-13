61a1063d8bc5bfb0af82126ce888cfc8c7909877
Cheng Sun <chengsun9@gmail.com>
2017-04-13 16:21:59 +0100

    update bench (add sieve)

---
+++ bench/sieve.ml
@@ -0,0 +1,49 @@
+(***********************************************************************)
+(*                                                                     *)
+(*                                OCaml                                *)
+(*                                                                     *)
+(*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
+(*                                                                     *)
+(*  Copyright 1996 Institut National de Recherche en Informatique et   *)
+(*  en Automatique.  All rights reserved.  This file is distributed    *)
+(*  under the terms of the Q Public License version 1.0.               *)
+(*                                                                     *)
+(***********************************************************************)
+
+(* Eratosthene's sieve *)
+
+(* interval min max = [min; min+1; ...; max-1; max] *)
+
+let rec interval min max =
+  if min > max then [] else min :: interval (min + 1) max
+
+
+(* filter p L returns the list of the elements in list L
+   that satisfy predicate p *)
+
+let rec filter p = function
+     []  -> []
+  | a::r -> if p a then a :: filter p r else filter p r
+
+
+(* Application: removing all numbers multiple of n from a list of integers *)
+
+let remove_multiples_of n x =
+  filter (fun m -> m mod n <> 0) x
+
+
+(* The sieve itself *)
+
+let sieve max =
+  let rec filter_again = function
+     [] -> []
+  | n::r as l ->
+      if n*n > max then l else n :: filter_again (remove_multiples_of n r)
+  in
+    filter_again (interval 2 max)
+
+let () =
+  let primes = sieve 50_000 in
+  let prime_sum = List.fold_right (fun x y -> x + y) primes 0 in
+  Printf.printf "%d\n" prime_sum;
+  assert (prime_sum = 121013308)
--- benchmark.sh
+++ benchmark.sh
@@ -6,11 +6,10 @@ name=$1
 
 echo ">>> Benchmarking ${name}..."
 cp ./bench/${name}.ml /tmp/test.ml
-./ocamlc -dlambda -target-liballocs -o /tmp/test.ocaml.exe -g /tmp/test.ml
+OCAMLRUNPARAM=b ./ocamlc -target-liballocs -o /tmp/test.ocaml.exe -g /tmp/test.ml
 ./ocamlc -o /tmp/test.ocamlnative.exe -g /tmp/test.ml
-#gcc -ggdb -std=c99 -Wall -I. -o /tmp/test /tmp/test.c ./stdlib_liballocs/*.c ./liballocs.c
-gcc -ggdb -O2 -std=c99 -Wall -Wno-unused -I. -o /tmp/test.gcc.exe /tmp/test.c ./liballocs.c
-clang -ggdb -O2 -std=c99 -Wall -Wno-unused -I. -o /tmp/test.clang.exe /tmp/test.c ./liballocs.c
+gcc -ggdb -O2 -march=native -std=c99 -Wall -Wno-unused -I. -o /tmp/test.gcc.exe /tmp/test.c ./stdlib_liballocs/*.c ./liballocs.c
+clang -ggdb -O2 -march=native -std=c99 -Wall -Wno-unused -I. -o /tmp/test.clang.exe /tmp/test.c ./stdlib_liballocs/*.c ./liballocs.c
 
 echo "TESTING GCC:"
 time /tmp/test.gcc.exe || true
