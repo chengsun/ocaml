b477d45805213cf3b721544b0634b643c2775ac9
Cheng Sun <chengsun9@gmail.com>
2017-05-16 03:49:48 +0100

    update bench script

--- benchmark.sh
+++ benchmark.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 ./build_liballocs.sh
 
 if [ -f $1 ]; then
@@ -26,52 +24,29 @@ clang -O1 -std=c99 -Wall -Wno-unused -I. -o /tmp/test.clang-O1.exe /tmp/test.c .
 clang -O2 -std=c99 -Wall -Wno-unused -I. -o /tmp/test.clang-O2.exe /tmp/test.c ./stdlib_liballocs/*.c ./liballocs_runtime.c -lm
 clang -O3 -std=c99 -Wall -Wno-unused -I. -o /tmp/test.clang-O3.exe /tmp/test.c ./stdlib_liballocs/*.c ./liballocs_runtime.c -lm
 
-echo "TESTING GCC:"
-t=$(/bin/time -f '%e' /tmp/test.gcc-O0.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,gcc-O0,$t,$?" >> /home/csun/project/ocaml/results
+function dotest {
+    echo "TESTING $1:"
+    t=$(/bin/time -f '%e' $2 3>&2 2>&1 1>&3-)
+    code=$?
+    if [ $code -eq 0 ]; then
+        echo "$name,$gitref,$1,$t,$?" >> /home/csun/project/ocaml/results
+    else
+        echo "$1 exited with $code"
+    fi
+}
 
-echo "TESTING CLANG:"
-t=$(/bin/time -f '%e' /tmp/test.clang-O0.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,clang-O0,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING GCC:"
-t=$(/bin/time -f '%e' /tmp/test.gcc-O1.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,gcc-O1,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING CLANG:"
-t=$(/bin/time -f '%e' /tmp/test.clang-O1.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,clang-O1,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING GCC:"
-t=$(/bin/time -f '%e' /tmp/test.gcc-O2.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,gcc-O2,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING CLANG:"
-t=$(/bin/time -f '%e' /tmp/test.clang-O2.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,clang-O2,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING GCC:"
-t=$(/bin/time -f '%e' /tmp/test.gcc-O3.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,gcc-O3,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING CLANG:"
-t=$(/bin/time -f '%e' /tmp/test.clang-O3.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,clang-O3,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING OCAML (bytecode):"
-t=$(/bin/time -f '%e' /home/csun/project/ocaml/installed/bin/ocamlrun /tmp/test.ocaml.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,ocaml,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING OCAML (native):"
-t=$(/bin/time -f '%e' /tmp/test.ocamlnative.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,ocamlopt,$t,$?" >> /home/csun/project/ocaml/results
+dotest gcc-O0 /tmp/test.gcc-O0.exe
+dotest clang-O0 /tmp/test.clang-O0.exe
+dotest gcc-O1 /tmp/test.gcc-O1.exe
+dotest clang-O1 /tmp/test.clang-O1.exe
+dotest gcc-O2 /tmp/test.gcc-O2.exe
+dotest clang-O2 /tmp/test.clang-O2.exe
+dotest gcc-O3 /tmp/test.gcc-O3.exe
+dotest clang-O3 /tmp/test.clang-O3.exe
+dotest ocaml "/home/csun/project/ocaml/installed/bin/ocamlrun /tmp/test.ocaml.exe"
+dotest ocamlopt /tmp/test.ocamlnative.exe
 
 export OCAMLRUNPARAM='s=4096M,space_overhead=1000000,max_overhead=1000000'
 
-echo "TESTING OCAML (bytecode no GC):"
-t=$(/bin/time -f '%e' /home/csun/project/ocaml/installed/bin/ocamlrun /tmp/test.ocaml.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,ocaml-nogc,$t,$?" >> /home/csun/project/ocaml/results
-
-echo "TESTING OCAML (native no GC):"
-t=$(/bin/time -f '%e' /tmp/test.ocamlnative.exe 3>&2 2>&1 1>&3-)
-echo "$name,$gitref,ocamlopt-nogc,$t,$?" >> /home/csun/project/ocaml/results
+dotest ocaml-nogc "/home/csun/project/ocaml/installed/bin/ocamlrun /tmp/test.ocaml.exe"
+dotest ocamlopt-nogc /tmp/test.ocamlnative.exe
