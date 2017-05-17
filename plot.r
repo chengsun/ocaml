323e175b03345ab3710aea02092bbb089f2322e2
Cheng Sun <chengsun9@gmail.com>
2017-05-17 08:50:55 +0100

    plot

--- plot.r
+++ plot.r
@@ -44,4 +44,23 @@ for (git_hash in git_hashes) {
     #ggplot(subset, aes(benchmark, speedup)) + geom_bar(aes(fill = compiler), position = "dodge", stat = "identity") + scale_y_log10(breaks=c(.1,.2,.3,.4,.5,.6,.7,.8,.9,1,2,3,4,5,6,7,8,9,10),labels=c(.1,.2,.3,.4,.5,.6,.7,.8,.9,1,2,3,4,5,6,7,8,9,10))
     ggplot(subset, aes(benchmark, normalised_times)) + geom_bar(aes(fill = compiler), position = "dodge", stat = "identity")
     ggsave(sprintf("resultsummary-%s.png", git_hash),width=29.7,height=21,units="cm",dpi=300)
+
+    plot_closure <- function(bench_type) {
+        subset <- data_averages[
+            data_averages$git_hash == git_hash &
+            data_averages$compiler %in% c("gcc-O3","clang-O3", "ocaml", "ocamlopt") &
+            startsWith(as.character(data_averages$benchmark), sprintf("closure_%s", bench_type)),
+        ]
+        subset$n <- sapply(subset$benchmark, function(s) {
+            strtoi(gsub(sprintf("^closure_%s_", bench_type),"",s), base=10)
+        })
+        ggplot(subset, aes(n, time, group=compiler, color=compiler)) + geom_line()
+        ggsave(sprintf("resultclosure_%s_summary-%s.png", bench_type, git_hash),width=29.7,height=21,units="cm",dpi=300)
+    }
+
+    plot_closure("capture")
+    plot_closure("create")
+    plot_closure("invoke")
 }
+
+
